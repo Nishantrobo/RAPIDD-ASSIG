f2b3ea784505b2fe0547a785e899b927fc845204
Nishantkumar <nishant9843.kumar@gmail.com>
2023-06-28 17:37:57 +0530

    assigment1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { Fragment } from 'react';
+import Header from './component/header';
+import Body from './component/body';
+
+
+
+function App() {
+
+  return (
+    <Fragment>
+      <Header />
+      <Body />
+    </Fragment>
+  )
+}
+
+export default App;
